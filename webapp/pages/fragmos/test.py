25adad23ed4a2eb6199320e39ce7fa6d4b69b76c
MikoNix <mikonix@hotmail.com>
2026-02-22 12:11:58 +0000

    modified:   webapp/pages/fragmos/Xuita.xml modified:   webapp/pages/fragmos/test.py deleted:    "webapp/static/templates/\320\250\320\260\320\261\320\273\320\276\320\275 \320\277\321\200\320\276\320\263.docx" deleted:    "webapp/static/templates/\320\250\320\260\320\261\320\273\320\276\320\275_\320\277\321\200\320\276\320\263_updated.docx"

--- webapp/pages/fragmos/test.py
+++ webapp/pages/fragmos/test.py
@@ -8,13 +8,15 @@ os.remove("/workspaces/Fragmos/webapp/pages/fragmos/Xuita.xml")
 
 # Блок начало / конец
 class Base(drawpyo.diagram.Object):  
-    def __init__(self, page,  value, x, y):
+    def __init__(self, page, value, x, y, type):
         super().__init__(page=page)
         self.value = value
         self.width = 120
         self.height = 40
         self.position = (x, y)
+        self.type = type
         self.apply_style_string("whiteSpace=wrap;rounded=1;dashed=0; whiteSpace=wrap; html=1; arcSize=50;arcSize=50;")
+        
 
 # Блок выполнения
 class Execute(drawpyo.diagram.Object):
@@ -26,22 +28,79 @@ class Execute(drawpyo.diagram.Object):
         self.position = (x, y)
         self.apply_style_string("rounded=0;whiteSpace=wrap;html=1;")
 
+# Блок условия
 class IF(drawpyo.diagram.Object):
     def __init__(self, page,  value, x, y):
         super().__init__(page=page)
         self.value = value
-        self.width = 120 * (( len(value) // 50 ) + 1)
-        self.height = 50 * (( len(value) // 50 ) + 1)
+        self.width = 200 * (( len(value) // 50 ) + 1)
+        self.height = 80 * (( len(value) // 50 ) + 1)
         self.position = (x, y)
         self.apply_style_string("whiteSpace=wrap;html=1;shape=rhombus;")
 
+# Блок цикла
+class For_default(drawpyo.diagram.Object):
+    def __init__(self, page,  value, x, y):
+        super().__init__(page=page)
+        self.value = value
+        self.width = 120 * (( len(value) // 50 ) + 1)
+        self.height = 40 * (( len(value) // 50 ) + 1)
+        self.position = (x, y)
+        self.apply_style_string("shape=hexagon;perimeter=hexagonPerimeter2;whiteSpace=wrap;html=1;fixedSize=1;")
+
+# Блок цикла с ограничением(начало)
+class Loop_limit_start(drawpyo.diagram.Object):
+    def __init__(self, page,  value, x, y):
+        super().__init__(page=page)
+        self.value = value
+        self.width = 120 * (( len(value) // 50 ) + 1)
+        self.height = 40 * (( len(value) // 50 ) + 1)
+        self.position = (x, y)
+        self.apply_style_string("shape=loopLimit;whiteSpace=wrap;html=1;")
+    
+# Блок цикла с ограничением (конец)
+class Loop_limit_end(drawpyo.diagram.Object):
+    def __init__(self, page,  value, x, y):
+        super().__init__(page=page)
+        self.value = value
+        self.width = 120 * (( len(value) // 50 ) + 1)
+        self.height = 40 * (( len(value) // 50 ) + 1)
+        self.position = (x, y)
+        self.apply_style_string("shape=loopLimit;whiteSpace=wrap;html=1;direction=west;")
+
+# Блок подпрограммы
+class Proccess(drawpyo.diagram.Object):
+    def __init__(self, page,  value, x, y):
+        super().__init__(page=page)
+        self.value = value
+        self.width = 120 * (( len(value) // 50 ) + 1)
+        self.height = 40 * (( len(value) // 50 ) + 1)
+        self.position = (x, y)
+        self.apply_style_string("shape=process;whiteSpace=wrap;html=1;backgroundOutline=1;")
+
 # Отрисовка стрелок
-class Pointer(drawpyo.diagram.Object):
-    pass
+
+class Pointer(drawpyo.diagram.Edge):
+    def __init__(self, page, source, target):
+        super().__init__(page=page,)
+        self.source = source
+        self.target = target
+        self.pointer_type()
+        
+    def pointer_type(self):
+        if self.source.position[1] > self.target.position[1]:
+            self.apply_style_string("endArrow=classic;html=1;rounded=0;waypoint=orthogonal;") 
+        if self.source.position[1] < self.target.position[1]:
+            self.apply_style_string("endArrow=none;html=1;rounded=0;waypoint=orthogonal;")
+    
 
 # TODO : класс форматирования текста 
 
-
+class Text_format(drawpyo.diagram.Object):
+    def __init__(self, page,  value, x, y):
+        super().__init__(page=page)
+        self.value = value
+        
 
 
 test = drawpyo.File()
@@ -51,10 +110,12 @@ test.file_name = "Xuita.xml"
 
 page = drawpyo.Page(file=test)
 
-text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n aaaaaaaaaaaaaaaaaaaaaaaa"
 
-base_obj  = Execute(page, value=text, x=100, y=100)
-base_obj1 = Base(page, value="123", x=100, y=200)
-base_obj2 = IF(page, value="if (sum + files[i] <= s)", x=100, y=300)
+base_obj = Base(page, "Начало", 100, 100, "start")
+base_obj2 = Base(page, "Конец", 100, 300, "end")
+point = Pointer(page, base_obj, base_obj2)
+base_obj3 = Base(page, "Начало 2", -250, 100, "start")
+point2 = Pointer(page, base_obj2, base_obj3)
+
 test.write()
 
